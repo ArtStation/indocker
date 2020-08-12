5057766eef86a8729933210a9eff6e89d25a6c4b
Nikita Kononov <vocrsz@gmail.com>
2020-08-12 17:31:47 +0300

    Update clonner.rb

--- lib/indocker/repositories/clonner.rb
+++ lib/indocker/repositories/clonner.rb
@@ -9,9 +9,16 @@ class Indocker::Repositories::Clonner
   def clone(session, repository)
     raise ArgumenError.new("only git repositories should be clonned") if !repository.is_git?
 
-    session.exec!("rm -rf #{repository.clone_path} && mkdir -p #{repository.clone_path}")
-    
-    git_command = "git clone -b #{repository.branch} --depth 1 #{repository.remote_url} #{repository.clone_path}"
+    repository_remote_url = session.exec!("mkdir -p #{repository.clone_path} && cd #{repository.clone_path} && git config --get remote.origin.url").stdout_data.chomp
+
+    @logger.debug("server remote_url => #{repository_remote_url.inspect}, indocker remote_url => #{repository.remote_url.inspect}")
+
+    git_command = if repository_remote_url == repository.remote_url
+      "cd #{repository.clone_path} && git add . && git reset HEAD --hard && git checkout #{repository.branch} && git pull --force"
+    else
+      "rm -rf #{repository.clone_path} && mkdir -p #{repository.clone_path} && git clone -b #{repository.branch} --depth 1 #{repository.remote_url} #{repository.clone_path}"
+    end
+
     session.exec!("ssh-agent bash -c 'ssh-add ~/.ssh/#{repository.ssh_key}; #{git_command}'")
   end
 end
