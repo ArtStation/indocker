8d929b85943d07d963af8efe0527d23a9e7938e0
Nikita Kononov <vocrsz@gmail.com>
2020-09-07 16:02:00 +0300

    Update clonner.rb

--- lib/indocker/repositories/clonner.rb
+++ lib/indocker/repositories/clonner.rb
@@ -9,15 +9,13 @@ class Indocker::Repositories::Clonner
   def clone(session, repository)
     raise ArgumenError.new("only git repositories should be clonned") if !repository.is_git?
 
-    repository_remote_url = session.exec!(
-      build_git_remote_url_command(
-        path: repository.clone_path
-      )
-    ).stdout_data.chomp
+    already_clonned = repository_already_clonned?(
+      session:     session,
+      target_path: repository.clone_path,
+      remote_url:  repository.remote_url,
+    )
 
-    @logger.debug("server remote_url:  #{repository_remote_url.inspect}, indocker remote_url: #{repository.remote_url.inspect}")
-
-    git_command = if repository_remote_url == repository.remote_url
+    git_command = if already_clonned
       build_force_pull_command(
         target_path: repository.clone_path,
         branch_name: repository.branch,
@@ -34,6 +32,18 @@ class Indocker::Repositories::Clonner
   end
 
   private
+
+    def repository_already_clonned?(session:, target_path:, remote_url:)
+      target_remote_url = session.exec!(
+        build_git_remote_url_command(
+          path: repository.clone_path
+        )
+      ).stdout_data.chomp
+
+      @logger.debug("target remote_url:  #{target_remote_url.inspect}, checked remote_url: #{remote_url.inspect}")
+
+      target_remote_url == remote_url
+    end
 
     def build_git_remote_url_command(path:)
       "mkdir -p #{path} && cd #{path} && git config --get remote.origin.url"
