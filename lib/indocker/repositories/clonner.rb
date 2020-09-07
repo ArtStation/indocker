65f013b49b9d755183b7b88509f31a4a3d6303a7
Nikita Kononov <vocrsz@gmail.com>
2020-09-07 15:49:56 +0300

    review changes

--- lib/indocker/repositories/clonner.rb
+++ lib/indocker/repositories/clonner.rb
@@ -9,16 +9,41 @@ class Indocker::Repositories::Clonner
   def clone(session, repository)
     raise ArgumenError.new("only git repositories should be clonned") if !repository.is_git?
 
-    repository_remote_url = session.exec!("mkdir -p #{repository.clone_path} && cd #{repository.clone_path} && git config --get remote.origin.url").stdout_data.chomp
+    repository_remote_url = session.exec!(
+      build_git_remote_url_command(
+        path: repository.clone_path
+      )
+    ).stdout_data.chomp
 
-    @logger.debug("server remote_url => #{repository_remote_url.inspect}, indocker remote_url => #{repository.remote_url.inspect}")
+    @logger.debug("server remote_url:  #{repository_remote_url.inspect}, indocker remote_url: #{repository.remote_url.inspect}")
 
     git_command = if repository_remote_url == repository.remote_url
-      "cd #{repository.clone_path} && git add . && git reset HEAD --hard && git checkout #{repository.branch} && git pull --force"
+      build_force_pull_command(
+        target_path: repository.clone_path,
+        branch_name: repository.branch,
+        remote_url:  repository.remote_url,
+      )
     else
-      "rm -rf #{repository.clone_path} && mkdir -p #{repository.clone_path} && git clone -b #{repository.branch} --depth 1 #{repository.remote_url} #{repository.clone_path}"
+      build_clone_command(
+        target_path: repository.clone_path,
+        branch_name: repository.branch,
+      )
     end
 
     session.exec!("ssh-agent bash -c 'ssh-add ~/.ssh/#{repository.ssh_key}; #{git_command}'")
   end
+
+  private
+
+    def build_git_remote_url_command(path:)
+      "mkdir -p #{path} && cd #{path} && git config --get remote.origin.url"
+    end
+
+    def build_clone_command(target_path:, branch_name:, remote_url:)
+      "rm -rf #{target_path} && mkdir -p #{target_path} && git clone -b #{branch_name} --depth 1 #{remote_url} #{target_path}"
+    end
+
+    def build_force_pull_command(target_path:, branch_name:)
+      "cd #{target_path} && git add . && git reset HEAD --hard && git checkout #{branch_name} && git pull --force"
+    end
 end
